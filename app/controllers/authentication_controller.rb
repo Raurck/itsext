aade17eee7195bf9a9a10021bc751df6beab665c
agkufko <agkufko@localhost.localdomain>
2017-06-11 11:43:26 +0500

    auth fixes

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,15 +4,16 @@ class AuthenticationController < ApplicationController
     if user.valid_password?(params[:password])
       render json: payload(user)
     else
-      render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
+      render json: {errors: ['Invalid Username/Password: wrong p[assword']}, status: :unauthorized
     end
   rescue
-    render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
+    render json: {errors: ['Invalid Username/Password: not found']}, status: :unauthorized
   end
 
   private
 
   def payload(user)
+
     return nil unless user and user.id
     {
       auth_token: JsonWebToken.encode({user_id: user.id}),
